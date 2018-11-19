0e7ca42329a94e2eb7f775b9220abf8a9635408a
Mei-Li Okumura <19okumuram@flintridgeprep.org>
2018-11-18 19:16:36 -0800

    generate random weight

--- TestCatList.java
+++ TestCatList.java
@@ -2,6 +2,17 @@ import java.util.*;
 public class TestCatList{
     public static void main(String[] args){
         ArrayList<Cat> catList = new ArrayList<Cat>();
-        
+        for (int i = 0; i < 4; i++){
+            catList.add(new Cat());
+        }
+        catList.get(0).setName("pumba");
+        catList.get(1).setName("mufasa");
+        catList.get(2).setName("simba");
+        catList.get(3).setName("timon");
+        for(Cat myCat: catList){
+            myCat.setWeight(Math.random()*10+5);
+        }
+        System.out.println(catList.get(0).getName() + "'s weight is " + catList.get(0).getWeight());
+
     }
 }
