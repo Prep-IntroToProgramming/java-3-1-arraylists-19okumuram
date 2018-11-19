11146a4bc816df5f45bed92eff3b49cb5d850e00
Mei-Li Okumura <19okumuram@flintridgeprep.org>
2018-11-18 18:25:17 -0800

    creating constructors

--- Cat.java
+++ Cat.java
@@ -2,11 +2,26 @@ public class Cat {
     private String name; //declaring private variable
     private double weight; //declaring private variable
     String getName(){
+        return name; // allows user to access name
     }
+
     void setName(String newName){
+        name = newName;
     }
+
     double getWeight(){
+        return weight; // allows user to access weight
     }
+
     void setWeight(double newWeight){
+        weight = newWeight;
+    }
+
+    public Cat (){
+
+    }
+
+    public Cat(String userName){
+        name = userName;
     }
 }
