d659bf52806fa877e95f84e1f188df25048b2288
Mei-Li Okumura <19okumuram@flintridgeprep.org>
2018-11-18 18:20:26 -0800

    creating setters/getters

--- Cat.java
+++ Cat.java
@@ -1,3 +1,12 @@
-
 public class Cat {
+    private String name; //declaring private variable
+    private double weight; //declaring private variable
+    String getName(){
+    }
+    void setName(String newName){
+    }
+    double getWeight(){
+    }
+    void setWeight(double newWeight){
+    }
 }
