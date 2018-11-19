8cb584a406c21fa678923b8e8e9e587773e537b4
Mei-Li Okumura <19okumuram@flintridgeprep.org>
2018-11-18 19:31:09 -0800

    bonus of changing meow method based on weight

--- Cat.java
+++ Cat.java
@@ -26,6 +26,14 @@ public class Cat {
     }
 
     public void meow(){
-        System.out.println("meow meow meow meow");
+        if (weight < 4){
+            System.out.println("meow");
+        } else if ((weight >= 4) && (weight < 8)){
+            System.out.println("meow meow");
+        } else if ((weight >= 8) && (weight < 12)){
+            System.out.println("meow meow meow. gimmme food");
+        } else {
+            System.out.println("MEOW MEOW MEOW MEOW I WANT FOOD HOOMAN");
+        }
     }
 }
